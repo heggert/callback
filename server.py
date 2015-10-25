7c24ac3cb0a35a790a8e49bc709ec92291c5a55d
sraval <sraval@twilio.com>
2015-10-24 23:14:17 -0700

    lol

--- server.py
+++ server.py
@@ -67,12 +67,37 @@ def welcome():
   return str(resp)
   
   
-@app.route('/enterqueue', methods=['GET', 'POST'])
-def enter():
-  resp = twilio.twiml.Response()
-  resp.say("You will join the wait queue")
-  resp.enqueue("wait")
-  return str(resp)
+# Configure this number to a toll-free Twilio number to accept incoming calls.
+@app.route('/caller', methods=['GET', 'POST'])
+def caller():
+    response = twiml.Response()
+    response.say("Thank you for calling this demonstration of Twilio Queue " \
+            "Please hold.")
+    response.enqueue("Queue Demo", waitUrl='/wait')
+    return str(response)
+
+
+# Configure waiting room to notify user of current position in the queue and
+# play the sweet, soothing sounds of Twilio's coffeeshop collection.
+@app.route('/wait', methods=['GET', 'POST'])
+def wait():
+    response = twiml.Response()
+    response.say("You are number %s in line." % request.form['QueuePosition'])
+    response.play("http://com.twilio.music.guitars.s3.amazonaws.com/" \
+            "Pitx_-_A_Thought.mp3")
+    response.play("http://com.twilio.music.guitars.s3.amazonaws.com/" \
+            "Pitx_-_Long_Winter.mp3")
+    response.redirect('/wait')
+    return str(response)
+
+
+# Connect to support queue - assign to Twilio number for agent to call.
+@app.route('/agent', methods=['GET', 'POST'])
+def agent():
+    response = twiml.Response()
+    with response.dial() as dial:
+        dial.queue("Queue Demo")
+    return str(response)
 
 
 if __name__ == "__main__":
